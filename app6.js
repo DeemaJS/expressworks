dcec870b6c8863c9e092d6cea09d3c3e2ac9122d
DeemaJS <deeema@outlook.com>
2015-10-18 16:49:36 +0300

    2 challenges left

---
+++ app6.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var crypto = require('crypto');
+
+app.put('/message/:id', function (req, res, next) {
+  var id = req.params.id;
+  res.send(crypto.createHash('sha1').update(new Date().toDateString() + id).digest('hex'));
+});
+
+app.listen(process.argv[2]);
